1464b662f347335f2e7fa2bea2b4fcf578b31930
gigatexal <alexandar.narayan@gmail.com>
2015-07-13 05:54:29 +0000

    fixes

--- euler/prob4.py
+++ euler/prob4.py
@@ -1,30 +1,5 @@
-"""
-palindrom
-999x999 eg. in two digits 99x901 
-99x91 = 9009
+possibles = max((x*y for x in xrange(100,1000) for y in xrange(x,1000) if str(x*y) == str(x*y)[::-1]))
 
-"""
-def is_palindrome(i):
-    return str(i) == str(i)[::-1]
+print possibles
 
-# what's the max?
-max_p = 0
-
-# multiply all numbers `i` between 100 and 998, with all between i+1 and 999
-for i in xrange(100, 999):
-    for j in xrange(i+1, 1000):
-        p = i * j
-        if is_palindrome(p) and p > max_p:
-            max_p = p
-
-print max_p
-
-				
-
-
-
-
-
-
-
-
+	
--- euler/prob4_gen.py
+++
@@ -1,5 +0,0 @@
-possibles = max((x*y for x in xrange(100,1000) for y in xrange(x,1000) if str(x*y) == str(x*y)[::-1]))
-
-print possibles
-
-	
