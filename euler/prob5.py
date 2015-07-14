cd051a4011b9fb5d48c53d06d873c21402ec4a41
gigatexal <alexandar.narayan@gmail.com>
2015-07-14 04:43:33 +0000

    finished #5

--- euler/prob5.py
+++ euler/prob5.py
@@ -1,19 +1,29 @@
 """
 if what is divided is even
+when divided it leaves no remainder
 """
-def isEven(x):
-	if x % 2 == 0:
+	
+def divs(x):
+	count = 0;
+	for i in xrange(1,21):
+		if x % i == 0:
+			count = count + 1
+	if count == 20:
 		return True
 
-def byTwenty(x):
-	for i in xrange(1,21):	
-		if not isEven(x % i):
-			return False
+	if count <> 20:
+		return False
+
+
+
+
+if __name__ == "__main__":
+	for i in xrange(20,9999999999):
+		if divs(i):
+			print i
 			break
-		#needs work:
-val = 0	
-for i in xrange(20,99999999):
-	found = byTwenty(i)
-	if found:
-		print i
+
+
+
+
 
